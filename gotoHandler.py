8d6463342666275e8a5904bd1febbe1c9ef48aad
mostafawkh <mt.khoshnood@gmail.com>
2023-02-01 17:34:54 +0330

    gotohandler implemented!

---
+++ gotoHandler.py
@@ -0,0 +1,19 @@
+def gotoHandler(state):
+    global statesDict, stateCount
+ 
+    # find all symbols on which we need to
+    # make function call - GOTO
+    generateStatesFor = []
+    for rule in statesDict[state]:
+        # if rule is not "Handle"
+        if rule[1][-1] != '.':
+            indexOfDot = rule[1].index('.')
+            dotPointsHere = rule[1][indexOfDot + 1]
+            if dotPointsHere not in generateStatesFor:
+                generateStatesFor.append(dotPointsHere)
+ 
+    # call GOTO iteratively on all symbols pointed by dot
+    if len(generateStatesFor) != 0:
+        for symbol in generateStatesFor:
+            GOTO(state, symbol)
+    return
